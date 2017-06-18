9f0c72cbe51d16807baae04af8d6283a0010f458
rNexeR <rnexer@gmail.com>
2017-06-18 16:53:07 -0600

    Ping v2

--- Firebird-C++/main.cpp
+++ Firebird-C++/main.cpp
@@ -1,21 +1,25 @@
-#include <iostream>
-#include <fcntl.h>
-#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/param.h>
 #include <sys/socket.h>
-#include <resolv.h>
-#include <netdb.h>
+#include <sys/file.h>
+#include <sys/time.h>
+#include <netinet/in_systm.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
-
-#define PACKETSIZE  64
-struct packet
-{
-    struct icmphdr hdr;
-    char msg[PACKETSIZE-sizeof(struct icmphdr)];
-};
-int pid=-1;
-struct protoent *proto=NULL;
-int cnt=1;
+#include <netdb.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <iostream>
+#define	DEFDATALEN	(64-ICMP_MINLEN)
+#define	MAXIPLEN	60
+#define	MAXICMPLEN	76
+#define	MAXPACKET	(65536 - 60 - ICMP_MINLEN)
 
 using namespace std;
 
@@ -61,90 +65,168 @@ int main()
     return 0;
 }
 
-unsigned short checksum(void *b, int len)
+uint16_t in_cksum(uint16_t *addr, unsigned len)
 {
-    unsigned short *buf = b;
-    unsigned int sum=0;
-    unsigned short result;
+  uint16_t answer = 0;
+  /*
+   * Algorithm is simple, using a 32 bit accumulator (sum), add
+   * sequential 16 bit words to it, and at the end, fold back all the
+   * carry bits from the t   16 bits into the lower 16 bits.
+   */
+  uint32_t sum = 0;
+  while (len > 1)  {
+    sum += *addr++;
+    len -= 2;
+  }
 
-    for ( sum = 0; len > 1; len -= 2 )
-        sum += *buf++;
-    if ( len == 1 )
-        sum += *(unsigned char*)buf;
-    sum = (sum >> 16) + (sum & 0xFFFF);
-    sum += (sum >> 16);
-    result = ~sum;
-    return result;
-}
+  if (len == 1) {
+    *(unsigned char *)&answer = *(unsigned char *)addr ;
+    sum += answer;
+  }
 
-int ping(char *adress)
-{
-    const int val=255;
-    int i, sd;
-    struct packet pckt;
-    struct sockaddr_in r_addr;
-    int loop;
-    struct hostent *hname;
-    struct sockaddr_in addr_ping,*addr;
-
-    pid = getpid();
-    proto = getprotobyname("ICMP");
-    hname = gethostbyname(adress);
-    bzero(&addr_ping, sizeof(addr_ping));
-    addr_ping.sin_family = hname->h_addrtype;
-    addr_ping.sin_port = 0;
-    addr_ping.sin_addr.s_addr = *(long*)hname->h_addr;
-
-    addr = &addr_ping;
-
-    sd = socket(PF_INET, SOCK_RAW, proto->p_proto);
-    if ( sd < 0 )
-    {
-        perror("socket");
-        return 1;
-    }
-    if ( setsockopt(sd, SOL_IP, IP_TTL, &val, sizeof(val)) != 0)
-    {
-        perror("Set TTL option");
-        return 1;
-    }
-    if ( fcntl(sd, F_SETFL, O_NONBLOCK) != 0 )
-    {
-        perror("Request nonblocking I/O");
-        return 1;
-    }
-
-    for (loop=0;loop < 10; loop++)
-    {
-
-        int len=sizeof(r_addr);
-
-        if ( recvfrom(sd, &pckt, sizeof(pckt), 0, (struct sockaddr*)&r_addr, &len) > 0 )
-        {
-            return 0;
-        }
-
-        bzero(&pckt, sizeof(pckt));
-        pckt.hdr.type = ICMP_ECHO;
-        pckt.hdr.un.echo.id = pid;
-        for ( i = 0; i < sizeof(pckt.msg)-1; i++ )
-            pckt.msg[i] = i+'0';
-        pckt.msg[i] = 0;
-        pckt.hdr.un.echo.sequence = cnt++;
-        pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
-        if ( sendto(sd, &pckt, sizeof(pckt), 0, (struct sockaddr*)addr, sizeof(*addr)) <= 0 )
-            perror("sendto");
-
-        usleep(300000);
-
-    }
-
-    return 1;
+  sum = (sum >> 16) + (sum & 0xffff);
+  sum += (sum >> 16);
+  answer = ~sum;
+  return answer;
 }
 
 int Ping(string url){
-    return ping((char*)url);
+    int s, i, cc, packlen, datalen = DEFDATALEN;
+	struct hostent *hp;
+	struct sockaddr_in to, from;
+	struct ip *ip;
+	u_char *packet, outpack[MAXPACKET];
+	char hnamebuf[MAXHOSTNAMELEN];
+	string hostname;
+	struct icmp *icp;
+	int ret, fromlen, hlen;
+	fd_set rfds;
+	struct timeval tv;
+	int retval;
+	struct timeval start, end;
+	int end_t;
+	bool cont = true;
+
+	to.sin_family = AF_INET;
+
+	to.sin_addr.s_addr = inet_addr(url.c_str());
+	if (to.sin_addr.s_addr != (u_int)-1)
+		hostname = url;
+	else
+	{
+		hp = gethostbyname(url.c_str());
+		if (!hp)
+		{
+			return 0;
+		}
+		to.sin_family = hp->h_addrtype;
+		bcopy(hp->h_addr, (caddr_t)&to.sin_addr, hp->h_length);
+		strncpy(hnamebuf, hp->h_name, sizeof(hnamebuf) - 1);
+		hostname = hnamebuf;
+	}
+	packlen = datalen + MAXIPLEN + MAXICMPLEN;
+	if ( (packet = (u_char *)malloc((u_int)packlen)) == NULL)
+	{
+		return 0;
+	}
+
+
+	if ( (s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
+	{
+		return 0; /* Needs to run as superuser!! */
+	}
+
+	icp = (struct icmp *)outpack;
+	icp->icmp_type = ICMP_ECHO;
+	icp->icmp_code = 0;
+	icp->icmp_cksum = 0;
+	icp->icmp_seq = 12345;
+	icp->icmp_id = getpid();
+
+
+	cc = datalen + ICMP_MINLEN;
+	icp->icmp_cksum = in_cksum((unsigned short *)icp,cc);
+
+	gettimeofday(&start, NULL);
+
+	i = sendto(s, (char *)outpack, cc, 0, (struct sockaddr*)&to, (socklen_t)sizeof(struct sockaddr_in));
+	if (i < 0 || i != cc)
+	{
+		if (i < 0)
+			perror("sendto error");
+	}
+
+	FD_ZERO(&rfds);
+	FD_SET(s, &rfds);
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+
+	while(cont)
+	{
+		retval = select(s+1, &rfds, NULL, NULL, &tv);
+		if (retval == -1)
+		{
+			perror("select()");
+			return 0;
+		}
+		else if (retval)
+		{
+			fromlen = sizeof(sockaddr_in);
+			if ( (ret = recvfrom(s, (char *)packet, packlen, 0,(struct sockaddr *)&from, (socklen_t*)&fromlen)) < 0)
+			{
+				perror("recvfrom error");
+				return 0;
+			}
+
+			// Check the IP header
+			ip = (struct ip *)((char*)packet);
+			hlen = sizeof( struct ip );
+			if (ret < (hlen + ICMP_MINLEN))
+			{
+				cerr << "packet too short (" << ret  << " bytes) from " << hostname << endl;;
+				return 0;
+			}
+
+			// Now the ICMP part
+			icp = (struct icmp *)(packet + hlen);
+			if (icp->icmp_type == ICMP_ECHOREPLY)
+			{
+				if (icp->icmp_seq != 12345)
+				{
+					cout << "received sequence # " << icp->icmp_seq << endl;
+					continue;
+				}
+				if (icp->icmp_id != getpid())
+				{
+					cout << "received id " << icp->icmp_id << endl;
+					continue;
+				}
+				cont = false;
+			}
+			else
+			{
+				cout << "Recv: not an echo reply" << endl;
+				continue;
+			}
+
+			gettimeofday(&end, NULL);
+			end_t = 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
+
+			if(end_t < 1)
+				end_t = 1;
+
+			cout << "Elapsed time = " << end_t << " usec" << endl;
+			return 1;
+		}
+		else
+		{
+			cout << "No data within one seconds.\n";
+			return 0;
+		}
+	}
+	return 0;
 }
+
 double PMT(double tasa, int n_periodos, double prestamo){
 
 }
