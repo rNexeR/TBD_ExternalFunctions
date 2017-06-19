68fd810fe90a78f3b5bcfcd9fda9798ce8cc5be3
rnexer <rnexer@gmail.com>
2017-06-18 20:06:33 -0600

    Ping linux version

--- Firebird-C++/main.cpp
+++ Firebird-C++/main.cpp
@@ -15,7 +15,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+
 #include <iostream>
+#include <math.h>
+#include <string>
+#include <bitset>
+
 #define	DEFDATALEN	(64-ICMP_MINLEN)
 #define	MAXIPLEN	60
 #define	MAXICMPLEN	76
@@ -32,7 +37,7 @@ double F2C(int fahrenheit);
 int Factorial(int number);
 string DEC2HEX(int number);
 int HEX2DEC(string hex);
-int CompareString(string str1, string str2);
+int Comparestring(string str1, string str2);
 string Trim(string str, char caracter);
 string Repeat(string repetir, int cantidad);
 
@@ -43,7 +48,7 @@ int main()
     cout<<("Ping: ");
     cout<<(Ping("www.google.com"))<<endl;
     cout<<("Str Compare: ");
-    cout<<(CompareString("c", "b"))<<endl;
+    cout<<(Comparestring("c", "b"))<<endl;
     cout<<("Factorial: ");
     cout<<(Factorial(10))<<endl;
     cout<<("C2F: ");
@@ -67,196 +72,244 @@ int main()
 
 uint16_t in_cksum(uint16_t *addr, unsigned len)
 {
-  uint16_t answer = 0;
-  /*
-   * Algorithm is simple, using a 32 bit accumulator (sum), add
-   * sequential 16 bit words to it, and at the end, fold back all the
-   * carry bits from the t   16 bits into the lower 16 bits.
-   */
-  uint32_t sum = 0;
-  while (len > 1)  {
-    sum += *addr++;
-    len -= 2;
-  }
+    uint16_t answer = 0;
+    /*
+     * Algorithm is simple, using a 32 bit accumulator (sum), add
+     * sequential 16 bit words to it, and at the end, fold back all the
+     * carry bits from the t   16 bits into the lower 16 bits.
+     */
+    uint32_t sum = 0;
+    while (len > 1)
+    {
+        sum += *addr++;
+        len -= 2;
+    }
 
-  if (len == 1) {
-    *(unsigned char *)&answer = *(unsigned char *)addr ;
-    sum += answer;
-  }
+    if (len == 1)
+    {
+        *(unsigned char *)&answer = *(unsigned char *)addr ;
+        sum += answer;
+    }
 
-  sum = (sum >> 16) + (sum & 0xffff);
-  sum += (sum >> 16);
-  answer = ~sum;
-  return answer;
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += (sum >> 16);
+    answer = ~sum;
+    return answer;
 }
 
-int Ping(string url){
+int Ping(string url)
+{
     int s, i, cc, packlen, datalen = DEFDATALEN;
-	struct hostent *hp;
-	struct sockaddr_in to, from;
-	struct ip *ip;
-	u_char *packet, outpack[MAXPACKET];
-	char hnamebuf[MAXHOSTNAMELEN];
-	string hostname;
-	struct icmp *icp;
-	int ret, fromlen, hlen;
-	fd_set rfds;
-	struct timeval tv;
-	int retval;
-	struct timeval start, end;
-	int end_t;
-	bool cont = true;
+    struct hostent *hp;
+    struct sockaddr_in to, from;
+    struct ip *ip;
+    u_char *packet, outpack[MAXPACKET];
+    char hnamebuf[MAXHOSTNAMELEN];
+    string hostname;
+    struct icmp *icp;
+    int ret, fromlen, hlen;
+    fd_set rfds;
+    struct timeval tv;
+    int retval;
+    struct timeval start, end;
+    int end_t;
+    bool cont = true;
 
-	to.sin_family = AF_INET;
+    to.sin_family = AF_INET;
 
-	to.sin_addr.s_addr = inet_addr(url.c_str());
-	if (to.sin_addr.s_addr != (u_int)-1)
-		hostname = url;
-	else
-	{
-		hp = gethostbyname(url.c_str());
-		if (!hp)
-		{
-			return 0;
-		}
-		to.sin_family = hp->h_addrtype;
-		bcopy(hp->h_addr, (caddr_t)&to.sin_addr, hp->h_length);
-		strncpy(hnamebuf, hp->h_name, sizeof(hnamebuf) - 1);
-		hostname = hnamebuf;
-	}
-	packlen = datalen + MAXIPLEN + MAXICMPLEN;
-	if ( (packet = (u_char *)malloc((u_int)packlen)) == NULL)
-	{
-		return 0;
-	}
-
-
-	if ( (s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
-	{
-		return 0; /* Needs to run as superuser!! */
-	}
-
-	icp = (struct icmp *)outpack;
-	icp->icmp_type = ICMP_ECHO;
-	icp->icmp_code = 0;
-	icp->icmp_cksum = 0;
-	icp->icmp_seq = 12345;
-	icp->icmp_id = getpid();
+    to.sin_addr.s_addr = inet_addr(url.c_str());
+    if (to.sin_addr.s_addr != (u_int)-1)
+        hostname = url;
+    else
+    {
+        hp = gethostbyname(url.c_str());
+        if (!hp)
+        {
+            return 0;
+        }
+        to.sin_family = hp->h_addrtype;
+        bcopy(hp->h_addr, (caddr_t)&to.sin_addr, hp->h_length);
+        strncpy(hnamebuf, hp->h_name, sizeof(hnamebuf) - 1);
+        hostname = hnamebuf;
+    }
+    packlen = datalen + MAXIPLEN + MAXICMPLEN;
+    if ( (packet = (u_char *)malloc((u_int)packlen)) == NULL)
+    {
+        return 0;
+    }
 
 
-	cc = datalen + ICMP_MINLEN;
-	icp->icmp_cksum = in_cksum((unsigned short *)icp,cc);
+    if ( (s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
+    {
+        return 0; /* Needs to run as superuser!! */
+    }
 
-	gettimeofday(&start, NULL);
+    icp = (struct icmp *)outpack;
+    icp->icmp_type = ICMP_ECHO;
+    icp->icmp_code = 0;
+    icp->icmp_cksum = 0;
+    icp->icmp_seq = 12345;
+    icp->icmp_id = getpid();
 
-	i = sendto(s, (char *)outpack, cc, 0, (struct sockaddr*)&to, (socklen_t)sizeof(struct sockaddr_in));
-	if (i < 0 || i != cc)
-	{
-		if (i < 0)
-			perror("sendto error");
-	}
 
-	FD_ZERO(&rfds);
-	FD_SET(s, &rfds);
-	tv.tv_sec = 1;
-	tv.tv_usec = 0;
+    cc = datalen + ICMP_MINLEN;
+    icp->icmp_cksum = in_cksum((unsigned short *)icp,cc);
 
-	while(cont)
-	{
-		retval = select(s+1, &rfds, NULL, NULL, &tv);
-		if (retval == -1)
-		{
-			perror("select()");
-			return 0;
-		}
-		else if (retval)
-		{
-			fromlen = sizeof(sockaddr_in);
-			if ( (ret = recvfrom(s, (char *)packet, packlen, 0,(struct sockaddr *)&from, (socklen_t*)&fromlen)) < 0)
-			{
-				perror("recvfrom error");
-				return 0;
-			}
+    gettimeofday(&start, NULL);
 
-			// Check the IP header
-			ip = (struct ip *)((char*)packet);
-			hlen = sizeof( struct ip );
-			if (ret < (hlen + ICMP_MINLEN))
-			{
-				cerr << "packet too short (" << ret  << " bytes) from " << hostname << endl;;
-				return 0;
-			}
+    i = sendto(s, (char *)outpack, cc, 0, (struct sockaddr*)&to, (socklen_t)sizeof(struct sockaddr_in));
+    if (i < 0 || i != cc)
+    {
+        if (i < 0)
+            perror("sendto error");
+    }
 
-			// Now the ICMP part
-			icp = (struct icmp *)(packet + hlen);
-			if (icp->icmp_type == ICMP_ECHOREPLY)
-			{
-				if (icp->icmp_seq != 12345)
-				{
-					cout << "received sequence # " << icp->icmp_seq << endl;
-					continue;
-				}
-				if (icp->icmp_id != getpid())
-				{
-					cout << "received id " << icp->icmp_id << endl;
-					continue;
-				}
-				cont = false;
-			}
-			else
-			{
-				cout << "Recv: not an echo reply" << endl;
-				continue;
-			}
+    FD_ZERO(&rfds);
+    FD_SET(s, &rfds);
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
 
-			gettimeofday(&end, NULL);
-			end_t = 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
+    while(cont)
+    {
+        retval = select(s+1, &rfds, NULL, NULL, &tv);
+        if (retval == -1)
+        {
+            return 0;
+        }
+        else if (retval)
+        {
+            fromlen = sizeof(sockaddr_in);
+            if ( (ret = recvfrom(s, (char *)packet, packlen, 0,(struct sockaddr *)&from, (socklen_t*)&fromlen)) < 0)
+            {
+                return 0;
+            }
 
-			if(end_t < 1)
-				end_t = 1;
+            // Check the IP header
+            ip = (struct ip *)((char*)packet);
+            hlen = sizeof( struct ip );
+            if (ret < (hlen + ICMP_MINLEN))
+            {
+                return 0;
+            }
 
-			cout << "Elapsed time = " << end_t << " usec" << endl;
-			return 1;
-		}
-		else
-		{
-			cout << "No data within one seconds.\n";
-			return 0;
-		}
-	}
-	return 0;
+            // Now the ICMP part
+            icp = (struct icmp *)(packet + hlen);
+            if (icp->icmp_type == ICMP_ECHOREPLY)
+            {
+                if (icp->icmp_seq != 12345)
+                {
+                    continue;
+                }
+                if (icp->icmp_id != getpid())
+                {
+                    continue;
+                }
+                cont = false;
+            }
+            else
+            {
+                cout << "Recv: not an echo reply" << endl;
+                continue;
+            }
+
+            gettimeofday(&end, NULL);
+            end_t = 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
+
+            if(end_t < 1)
+                end_t = 1;
+
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    return 0;
 }
 
-double PMT(double tasa, int n_periodos, double prestamo){
-
+double PMT(double tasa, int n_periodos, double prestamo)
+{
+    return (prestamo*tasa) / (1 - pow((1 + tasa),(-n_periodos)));
 }
-int BIN2DEC(string binary){
 
+int BIN2DEC(string binary)
+{
+    return bitset<8>(binary).to_ulong();
 }
-string DEC2BIN(int number){
 
+string DEC2BIN(int number)
+{
+    return bitset<8>(number).to_string();
 }
-double C2F(int celsius){
 
+double C2F(int celsius)
+{
+    return (9*celsius/5) + 32;
 }
-double F2C(int fahrenheit){
 
+double F2C(int fahrenheit)
+{
+    return ((fahrenheit - 32)*5)/9;
 }
-int Factorial(int number){
 
+int Factorial(int number)
+{
+    int ret = 1;
+    for(int i = 1; i <= number; i++)
+        ret *= i;
+    return ret;
 }
-string DEC2HEX(int number){
 
+string DEC2HEX(int number)
+{
+    char ret[20];
+    sprintf(ret,"%x",number);
+    return (string)ret;
 }
-int HEX2DEC(string hex){
 
+int HEX2DEC(string hex_val)
+{
+    int number = (int)strtol(hex_val.c_str(), NULL, 16);
+    return number;
 }
-int CompareString(string str1, string str2){
 
+int Comparestring(string str1, string str2)
+{
+    return str1.compare(str2);
 }
-string Trim(string str, char caracter){
 
+string Trim(string str, char caracter)
+{
+    string temp = "";
+    string ret = "";
+    bool check = true;
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(check && str[i] == caracter)
+            continue;
+        temp += str[i];
+        check = false;
+    }
+    ret = temp;
+
+    for(int i = temp.length() -1; i >= 0; i--)
+    {
+        if(temp[i] == caracter)
+        {
+            ret = ret.substr(0, i);
+            continue;
+        }
+        break;
+    }
+    return ret;
 }
-string Repeat(string repetir, int cantidad){
 
+string Repeat(string repetir, int cantidad)
+{
+    string ret = "";
+    for(int i = 0; i < cantidad; i++)
+        ret += repetir;
+    return ret;
 }
