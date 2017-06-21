fe8bcaf5fd0c57e8fb5943473d0e5d4aabf79bdb
rNexeR <rnexer@gmail.com>
2017-06-20 23:51:34 -0600

    last

--- SQLite-C#/ExternalFunctions/Program.cs
+++ SQLite-C#/ExternalFunctions/Program.cs
@@ -1,146 +1,265 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using System.Net;
+using System.Threading.Tasks;
+using System.Data.SQLite;
 using System.Net.NetworkInformation;
 
 namespace ExternalFunctions
 {
     class Program
     {
-        public static void Main(String[] args)
+        [SQLiteFunction(Name = "TRIM", Arguments = 2, FuncType = FunctionType.Scalar)]
+        public class TrimSQLiteFunction : SQLiteFunction
         {
-            Console.Write("Repeat: ");
-            Console.WriteLine(Repeat("Nexer ", 5));
-            Console.Write("Ping: ");
-            Console.WriteLine(Ping("www.google.com"));
-            Console.Write("Str Compare: ");
-            Console.WriteLine(CompareString("c", "b"));
-            Console.Write("Factorial: ");
-            Console.WriteLine(Factorial(10));
-            Console.Write("C2F: ");
-            Console.WriteLine(C2F(30));
-            Console.Write("F2C: ");
-            Console.WriteLine(F2C(86));
-            Console.Write("Trim: ");
-            Console.WriteLine(Trim("aaaaaHolasaaaaa", 'a'));
-            Console.Write("PMT: ");
-            Console.WriteLine(PMT(0.5, 10, 10000));
-            Console.Write("BIN2DEC: ");
-            Console.WriteLine(BIN2DEC("111"));
-            Console.Write("DEC2BIN: ");
-            Console.WriteLine(DEC2BIN(7));
-            Console.Write("DEC2HEX: ");
-            Console.WriteLine(DEC2HEX(15));
-            Console.Write("HEX2DEC: ");
-            Console.WriteLine(HEX2DEC("10"));
-            Console.ReadKey();
+            public override object Invoke(object[] args)
+            {
+                String str = args[0].ToString();
+                char caracter = args[1].ToString()[0];
+                String temp = "";
+                String ret = "";
+                bool check = true;
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (check && str[i] == caracter)
+                        continue;
+                    temp += str[i];
+                    check = false;
+                }
+                ret = temp;
+
+                for (int i = temp.Length - 1; i >= 0; i--)
+                {
+                    if (temp[i] == caracter)
+                    {
+                        ret = ret.Substring(0, i);
+                        continue;
+                    }
+                    break;
+                }
+                return ret;
+            }
         }
 
-        public static int Ping(String url)
+        [SQLiteFunction(Name = "REPEAT", Arguments = 2, FuncType = FunctionType.Scalar)]
+        public class RepeatSQLiteFunction : SQLiteFunction
         {
-            Ping pingSender = new Ping();
-            PingOptions options = new PingOptions();
-            options.DontFragment = true;
-
-            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            int timeout = 120;
-            try
+            public override object Invoke(object[] args)
             {
-                PingReply reply = pingSender.Send(url, timeout, buffer, options);
-                if (reply.Status == IPStatus.Success)
+                string repetir = args[0].ToString();
+                int cantidad = int.Parse(args[1].ToString());
+
+                String ret = "";
+                for (int i = 0; i < cantidad; i++)
+                    ret += repetir;
+                return ret;
+            }
+        }
+
+        [SQLiteFunction(Name = "COMPARESTRING", Arguments = 2, FuncType = FunctionType.Scalar)]
+        public class CompareStringSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                string str1 = args[0].ToString();
+                string str2 = args[1].ToString();
+
+                return str1.CompareTo(str2);
+            }
+        }
+
+        [SQLiteFunction(Name = "DEC2HEX", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class Dec2HexSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                int number = int.Parse(args[0].ToString());
+                return Convert.ToString(number, 16);
+            }
+        }
+
+        [SQLiteFunction(Name = "HEX2DEC", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class Hex2DecSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                string value = args[0].ToString();
+                int decValue = int.Parse(value, System.Globalization.NumberStyles.HexNumber);
+                return decValue.ToString();
+            }
+        }
+
+        [SQLiteFunction(Name = "FACTORIAL", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class FactorialSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                int number = int.Parse(args[0].ToString());
+                int ret = 1;
+                for (int i = 1; i <= number; i++)
+                    ret *= i;
+                return ret;
+            }
+        }
+
+        [SQLiteFunction(Name = "C2F", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class C2FSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                double celsius = Convert.ToDouble(args[0].ToString());
+                return (9 * celsius / 5) + 32;
+            }
+        }
+
+        [SQLiteFunction(Name = "F2C", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class F2CSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                double fahrenheit = Convert.ToDouble(args[0].ToString());
+                return ((fahrenheit - 32) * 5) / 9;
+            }
+        }
+
+        [SQLiteFunction(Name = "DEC2BIN", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class Dec2BinSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                int number = int.Parse(args[0].ToString());
+                return Convert.ToString(number, 2);
+            }
+        }
+
+        [SQLiteFunction(Name = "BIN2DEC", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class Bin2DecSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                String binary = args[0].ToString();
+                return Convert.ToInt32(binary, 2);
+            }
+        }
+
+        [SQLiteFunction(Name = "PMT", Arguments = 3, FuncType = FunctionType.Scalar)]
+        public class PmtSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                double tasa = Double.Parse(args[0].ToString());
+                int n_periodos = int.Parse(args[1].ToString());
+                double prestamo = Double.Parse(args[2].ToString());
+                return (prestamo * tasa) / (1 - Math.Pow((1 + tasa), (-n_periodos)));
+            }
+        }
+
+        [SQLiteFunction(Name = "PING", Arguments = 1, FuncType = FunctionType.Scalar)]
+        public class PingSQLiteFunction : SQLiteFunction
+        {
+            public override object Invoke(object[] args)
+            {
+                String url = args[0].ToString();
+                Ping pingSender = new Ping();
+                PingOptions options = new PingOptions();
+                options.DontFragment = true;
+
+                string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 120;
+                try
                 {
-                    return 1;
+                    PingReply reply = pingSender.Send(url, timeout, buffer, options);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        return 1;
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+
+                return 0;
+            }
+        }
+
+        static void BindFunction(SQLiteConnection connection, SQLiteFunction function)
+        {
+            var attributes = function.GetType().GetCustomAttributes(typeof(SQLiteFunctionAttribute), true).Cast<SQLiteFunctionAttribute>().ToArray();
+            if (attributes.Length == 0)
+            {
+                throw new InvalidOperationException("SQLiteFunction doesn't have SQLiteFunctionAttribute");
+            }
+            connection.BindFunction(attributes[0], function);
+        }
+
+        static void Main(string[] args)
+        {
+
+            var conn = new SQLiteConnection("Data Source=C:\\sqlite\\test.db");
+            conn.Open();
+
+            BindFunction(conn, new PmtSQLiteFunction());
+            BindFunction(conn, new PingSQLiteFunction());
+            BindFunction(conn, new Bin2DecSQLiteFunction());
+            BindFunction(conn, new Dec2BinSQLiteFunction());
+            BindFunction(conn, new F2CSQLiteFunction());
+            BindFunction(conn, new C2FSQLiteFunction());
+            BindFunction(conn, new FactorialSQLiteFunction());
+            BindFunction(conn, new Dec2HexSQLiteFunction());
+            BindFunction(conn, new Hex2DecSQLiteFunction());
+            BindFunction(conn, new Hex2DecSQLiteFunction());
+            BindFunction(conn, new CompareStringSQLiteFunction());
+            BindFunction(conn, new RepeatSQLiteFunction());
+            BindFunction(conn, new TrimSQLiteFunction());
+
+            while (true)
+            {
+                try
+                {
+                    string sql = Console.ReadLine().ToLower();
+
+                    string[] split = sql.Split(null);
+
+                    if (split.Length == 0)
+                        continue;
+
+                    SQLiteCommand command = new SQLiteCommand(sql, conn);
+                    if (split[0] == "select")
+                    {
+                        SQLiteDataReader reader = command.ExecuteReader();
+                        PrintReader(reader);
+                    }
+                    else
+                        command.ExecuteNonQuery();
+
+
+                }
+                catch (Exception e)
+                {
+
                 }
             }
-            catch (Exception ex)
+        }
+
+        private static void PrintReader(SQLiteDataReader reader)
+        {
+            while (reader.Read())
             {
-            }
-
-            return 0;
-        }
-
-        public static double PMT(double tasa, int n_periodos, double prestamo)
-        {
-            return (prestamo * tasa) / (1 - Math.Pow((1 + tasa), (-n_periodos)));
-        }
-
-        public static int BIN2DEC(String binary)
-        {
-            return Convert.ToInt32(binary, 2);
-        }
-
-        public static String DEC2BIN(int number)
-        {
-            return Convert.ToString(number, 2);
-        }
-
-        public static double C2F(double celsius)
-        {
-            return (9 * celsius / 5) + 32;
-        }
-
-        public static double F2C(double fahrenheit)
-        {
-            return ((fahrenheit - 32) * 5) / 9;
-        }
-
-        public static int Factorial(int number)
-        {
-            int ret = 1;
-            for (int i = 1; i <= number; i++)
-                ret *= i;
-            return ret;
-        }
-
-        public static String DEC2HEX(int number)
-        {
-            return Convert.ToString(number, 16);
-        }
-
-        public static int HEX2DEC(String hexa)
-        {
-            return Convert.ToInt32(hexa, 16);
-        }
-
-        public static int CompareString(String str1, String str2)
-        {
-            return str1.CompareTo(str2);
-        }
-
-        public static String Trim(String str, char caracter)
-        {
-            String temp = "";
-            String ret = "";
-            bool check = true;
-
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (check && str[i] == caracter)
-                    continue;
-                temp += str[i];
-                check = false;
-            }
-            ret = temp;
-
-            for (int i = temp.Length - 1; i >= 0; i--)
-            {
-                if (temp[i] == caracter)
+                for (int i = 0; i < reader.FieldCount; ++i)
                 {
-                    ret = ret.Substring(0, i);
-                    continue;
+                    var column_name = reader.GetName(i).ToString();
+                    Console.Write(column_name + ": " + reader[column_name] + " ");
                 }
-                break;
+                Console.WriteLine();
             }
-            return ret;
+
+
+            Console.Read();
         }
 
-        public static String Repeat(String repetir, int cantidad)
-        {
-            String ret = "";
-            for (int i = 0; i < cantidad; i++)
-                ret += repetir;
-            return ret;
-        }
+
     }
 }
